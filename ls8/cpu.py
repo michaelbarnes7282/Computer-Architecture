ae416900c8d2b8c086046ceb1fd410c98e4b5e2e
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-08-19 13:37:25 -0700

    finished WED MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,70 @@
 
 import sys
 
+LDI = "0b10000010"
+PUSH = "0b01000101"
+POP = "0b01000110"
+PRN = "0b01000111"
+HLT = "0b00000001"
+MULT = "0b10100010"
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 64
+        self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
         self.running = True
+        self.reg[7] = 0xf4
+
+        self.branchtable = {}
+        self.branchtable[LDI] = self.LDI
+        self.branchtable[PUSH] = self.PUSH
+        self.branchtable[POP] = self.POP
+        self.branchtable[PRN] = self.PRN
+        self.branchtable[HLT] = self.HLT
+        self.branchtable[MULT] = self.MUL
+
+    def HLT(self):
+        self.running = False
+        self.pc += 1
+
+    def LDI(self):
+        reg_num = int(self.ram[self.pc + 1], 2)
+        value = int(self.ram[self.pc + 2], 2)
+        self.reg[reg_num] = value
+        self.pc += 3
+
+    def PRN(self):
+        reg_num = int(self.ram[self.pc + 1], 2)
+        print(self.reg[reg_num])
+        self.pc += 2
+
+    def MUL(self):
+        reg1_num = int(self.ram[self.pc + 1], 2)
+        reg2_num = int(self.ram[self.pc + 2] ,2)
+        self.reg[reg1_num] *= self.reg[reg2_num]
+        self.pc += 3
+
+    def PUSH(self):
+        self.reg[7] -= 1
+        reg_num = int(self.ram[self.pc + 1], 2)
+        value = self.reg[reg_num]
+
+        top_of_stack_addr = self.reg[7]
+        self.ram[top_of_stack_addr] = value
+
+        self.pc += 2
+
+    def POP(self):
+        reg_num = int(self.ram[self.pc + 1], 2)
+        value = self.ram[self.reg[7]]
+        self.reg[reg_num] = value
+        self.reg[7] += 1
+        self.pc += 2
 
     def ram_read(self, pc):
         return self.ram[pc]
@@ -25,27 +80,55 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        if len(sys.argv) != 2:
+            print("usage: cpu.py progname")
+            sys.exit(1)
 
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    line = line.strip()
+                    temp = line.split()
+
+                    if len(temp) == 0:
+                        continue
+
+                    if temp[0][0] == '#':
+                        continue
+
+                    try:
+                        self.ram[address] = format(int(temp[0], 2), '#010b')
+
+                    except ValueError:
+                        print(f"Invalid number: {temp[0]}")
+                        sys.exit(1)
+
+                    address += 1
+
+        except FileNotFoundError:
+            print(f"Couldn't open {sys.argv[1]}")
+            sys.exit(2)
+
+        if address == 0:
+            print("Program was empty!")
+            sys.exit(3)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -57,8 +140,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -73,19 +156,5 @@ class CPU:
         """Run the CPU."""
         while self.running:
             ir = self.ram[self.pc]
-            if ir == 0b10000010:
-                print('we hit LDI')
-                reg_num = self.ram[self.pc + 1]
-                value = self.ram[self.pc + 2]
-                self.reg[reg_num] = value
-                self.pc += 3
-            elif ir == 0b01000111:
-                print('We hit PRN')
-                reg_num = self.ram[self.pc + 1]
-                print(" %02X" % self.reg[reg_num], end='')
-                self.pc += 2
-            elif ir == 0b00000001:
-                self.running = False
-                self.pc += 1
-
-
+            self.branchtable[ir]()
+            
